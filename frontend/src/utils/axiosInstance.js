3840e9525f8ad0e725120e1225566ef79c519f68
prince776 <princemit776@gmail.com>
2020-11-28 01:58:51 +0530

    Changed baseurls

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://codechefprogrammingpractice.000webhostapp.com',
     /* other custom settings */
 });
 
